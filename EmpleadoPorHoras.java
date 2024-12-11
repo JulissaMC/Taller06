bc640f972e8c2a2dc153f70025bf92548e41d378
Janeth Molina <127567902+JulissaMC@users.noreply.github.com>
2024-12-11 16:49:08 -0500

    Move method EmpleadoPorHoras.java

--- EmpleadoPorHoras.java
+++ EmpleadoPorHoras.java
@@ -14,10 +14,22 @@ public class EmpleadoPorHoras extends Empleado {
         System.out.println("Departamento: " + super.getDepartamento().getNombre());
     }
 
-    @Override
     public double calcularSalario() {
-        double salario = super.getHorasTrabajadas() * super.getTarifaHora();
-        return salario;
+        double salarioTotal = salarioBase;
+        if (horasTrabajadas < 0 ){
+            throw new IllegalArgumentException("Las horas trabajadas deben ser mayor o igual a 0");
+        }
+
+        if (salarioBase < 0 ){
+            throw new IllegalArgumentException("El salario debe ser mayor o igual a 0");
+        }
+
+        if (horasTrabajadas > 40){
+            salarioTotal += (horasTrabajadas - 40) * 50; // Pago de horas extra
+        }
+
+        return salarioTotal += departamento.AddSalario();
+        
     }
 
     // Más metodos
